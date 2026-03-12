b749a45963ef68f1afd7e12273134ce6aca62af9
alprkn123 <alldeor715@gmail.com>
2026-03-12 10:42:20 +0700

    First commit - Instagram bot

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/container
+mkdir -p models
+cd models
+wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+rm vosk-model-small-en-us-0.15.zip
+cd ..
+pip install -r requirements.txt
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/container
+pip install -r requirements.txt
+python bot.py
